1b06531809beec94a92ebb154ba1398e4947e738
Andreas Rammhold <andreas@rammhold.de>
2017-03-13 13:42:54 +0100

    Added profiling to main.go

--- main.go
+++ main.go
@@ -7,12 +7,29 @@ import (
     "github.com/andir/lightsd/core"
     _ "github.com/andir/lightsd/operations"
     "github.com/andir/lightsd/outputs/shm"
+    "runtime/pprof"
+    "os"
+    "fmt"
+    "os/signal"
 )
 
 func main() {
     configPath := flag.String("config", "config.yml", "The config file")
+    profileOutput := flag.String("cpuprofile", "", "Output file for profile output")
 
     flag.Parse()
+
+    if *profileOutput != "" {
+        fh, err := os.Create(*profileOutput)
+
+        if err != nil {
+            panic(err)
+        }
+        defer fh.Close()
+
+        pprof.StartCPUProfile(fh)
+        defer pprof.StopCPUProfile()
+    }
 
     config, err := LoadConfig(*configPath)
     if err != nil {
@@ -25,22 +42,31 @@ func main() {
         pipeline.NewOperation(op.Type, op.Name, config.Size, op.Config)
     }
 
-    NewMqttConnection(config, pipeline)
+    //NewMqttConnection(config, pipeline)
 
     bc := StartDebug()
     sink := shm.NewSHMOutput("/test", config.Size)
 
     interval := time.Second / time.Duration(config.FPS)
 
-    for {
-        elapsed := pipeline.Render()
+    go func() {
+        for {
+            elapsed := pipeline.Render()
 
-        result := pipeline.Result()
+            result := pipeline.Result()
 
-        sink.Render(result)
-        bc.Broadcast(result)
+            sink.Render(result)
+            bc.Broadcast(result)
 
-        diff := interval - elapsed
-        time.Sleep(diff)
+            diff := interval - elapsed
+            time.Sleep(diff)
+        }
+    }()
+
+    signalChan := make(chan os.Signal, 1)
+    signal.Notify(signalChan, os.Interrupt)
+    for _ = range signalChan {
+        fmt.Println("Interrupt received. Stopping")
+        return
     }
 }
